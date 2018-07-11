6b9de486d088b6dd746d34e55a1d52bee956c277
takubokudori <takubokudori@gmail.com>
2018-07-11 18:50:28 +0900

    first commit

---
+++ bluesc/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Errorprint = New System.Windows.Forms.Label()
+        Me.Qr = New System.Windows.Forms.PictureBox()
+        Me.InfoNavi = New System.Windows.Forms.Label()
+        Me.Prog = New System.Windows.Forms.Label()
+        CType(Me.Qr, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Errorprint
+        '
+        Me.Errorprint.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Errorprint.Font = New System.Drawing.Font("Segoe UI Symbol", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Errorprint.ForeColor = System.Drawing.SystemColors.Control
+        Me.Errorprint.Location = New System.Drawing.Point(-45, 273)
+        Me.Errorprint.Name = "Errorprint"
+        Me.Errorprint.Size = New System.Drawing.Size(776, 377)
+        Me.Errorprint.TabIndex = 4
+        '
+        'Qr
+        '
+        Me.Qr.BackColor = System.Drawing.Color.Blue
+        Me.Qr.Image = CType(resources.GetObject("Qr.Image"), System.Drawing.Image)
+        Me.Qr.Location = New System.Drawing.Point(102, 258)
+        Me.Qr.Name = "Qr"
+        Me.Qr.Size = New System.Drawing.Size(115, 117)
+        Me.Qr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Qr.TabIndex = 5
+        Me.Qr.TabStop = False
+        '
+        'InfoNavi
+        '
+        Me.InfoNavi.BackColor = System.Drawing.Color.Red
+        Me.InfoNavi.Font = New System.Drawing.Font("Segoe UI Symbol", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.InfoNavi.ForeColor = System.Drawing.SystemColors.Control
+        Me.InfoNavi.Location = New System.Drawing.Point(97, 130)
+        Me.InfoNavi.Name = "InfoNavi"
+        Me.InfoNavi.Size = New System.Drawing.Size(850, 179)
+        Me.InfoNavi.TabIndex = 6
+        Me.InfoNavi.Text = "システム"
+        '
+        'Prog
+        '
+        Me.Prog.AutoSize = True
+        Me.Prog.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Prog.Font = New System.Drawing.Font("Segoe UI Symbol", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Prog.ForeColor = System.Drawing.SystemColors.Control
+        Me.Prog.Location = New System.Drawing.Point(200, 138)
+        Me.Prog.Name = "Prog"
+        Me.Prog.Size = New System.Drawing.Size(121, 30)
+        Me.Prog.TabIndex = 7
+        Me.Prog.Text = "100% 完了"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(16, Byte), Integer), CType(CType(113, Byte), Integer), CType(CType(170, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Prog)
+        Me.Controls.Add(Me.InfoNavi)
+        Me.Controls.Add(Me.Qr)
+        Me.Controls.Add(Me.Errorprint)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Qr, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Errorprint As Label
+    Friend WithEvents Qr As PictureBox
+    Friend WithEvents InfoNavi As Label
+    Friend WithEvents Prog As Label
+End Class
---
+++ bluesc/Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+
+    Dim strMess As String
+    Dim strInfo As String
+    Dim len As Integer
+    Dim now As Integer
+    Dim rnd As New System.Random()
+    Dim num As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        strMess = "問題が発生したため、PC を再起動する必要があります。" & vbCr _
+            & "エラー情報を収集しています。多分自動的に再起動します。"
+        len = strMess.Length
+        strInfo = "この問題と可能な解決方法の詳細については、https://www.microsoft.com/stopcode を参照してください。" & vbCr & vbCr _
+            & "サポート担当者に連絡する場合は、この情報を伝えてください:" & vbCr _
+            & "停止コード: "
+        Dim stopCodes() As String = {"SCREEN_SAVER", "MEMORY_MANAGEMENT", "SET_OF_INVALID_CONTEXT", "SYSTEM_SERVICE_EXCEPTION", "IRQL_GT_ZERO_AT_SYSTEM_SERVICE", "STREAMS_INTERNAL_ERROR"}
+
+        strInfo = strInfo & stopCodes(rnd.Next(0, stopCodes.Length))
+        now = 0
+        Me.WindowState = FormWindowState.Maximized
+
+        Dim h As Integer = Me.Height ' ディスプレイ縦幅
+        Dim w As Integer = Me.Width ' ディスプレイ横幅
+        Dim offsetL As Integer = w * 0.114845 ' 左端のオフセット
+        Dim offsetU As Integer = offsetL / 2 ' 上端のオフセット
+        Dim offsetR As Integer = offsetL ' 右端のオフセット
+        Dim bc As Object = Color.FromArgb(16, 113, 170) ' 背景色
+        Dim fontsizept As Integer = h \ 40 ' 案内以外のフォントサイズ(pt)
+        Dim navifontsizept As Integer = fontsizept * 0.8125
+        Dim pimargine As Integer = w \ 70 ' pictureとinfo間のマージンサイズ
+
+        setAll(Errorprint, New Point(offsetL, offsetU), Me.Size, fontsizept, bc)
+        setAll(Prog, New Point(offsetL, offsetU + topx(fontsizept) * 5), Prog.Size, fontsizept, bc)
+
+        Qr.BackColor = bc
+        Qr.Size = New Size(h / 4, w / 8)
+        Qr.Location = New Point(offsetL, h / 2 - Qr.Size.Height / 2)
+        setAll(InfoNavi, New Point(offsetL + Qr.Width + pimargine, Qr.Location.Y), New Size(w - (offsetL + Qr.Width + pimargine + offsetR), InfoNavi.Height * 2), navifontsizept, bc)
+
+        Errorprint.Text = strMess
+        InfoNavi.Text = strInfo
+        updateProg(0)
+    End Sub
+
+    Function topx(pt As Integer) As Integer
+        ' pt to px
+        topx = (pt * 4) \ 3
+    End Function
+
+    Function topt(px As Integer) As Integer
+        topt = (px * 3) \ 4
+    End Function
+
+    Sub setAll(ByRef obj As Label, loc As Point, size As Size, fontsizept As Integer, bc As Object)
+        With obj
+            .BackColor = bc
+            .Location = New Point(loc)
+            .Font = New Font(.Font.FontFamily, fontsizept, .Font.Style)
+            .Size = New Size(size)
+        End With
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Try
+            updateProg(num + rnd.Next(1, 5))
+        Catch ex As OverflowException
+            updateProg(-num + rnd.Next(1, 5))
+        End Try
+        Timer1.Interval = rnd.Next(634, 2776)
+    End Sub
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+        Me.Close()
+    End Sub
+
+    Private Sub updateProg(a As Integer)
+        num = a
+        'If a < 100 Then
+        'num = a
+        'Else
+        ' num = 100 - rnd.Next(1, 10)
+        'End If
+        Prog.Text = Str(num) + "% 完了"
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Me.Close()
+    End Sub
+End Class
